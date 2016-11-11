46effeee6e8862c64841795e540cbb0ed568fe29
Keith J. Jones <keith@keithjjones.com>
2016-11-11 14:01:44 -0500

    Updated version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='visualize_logs',
-    version='20161106.1',
+    version='20161111.1',
     author='Keith J. Jones',
     author_email='keith@keithjjones.com',
     packages=['Visualize_Logs', 'Visualize_Logs.objects'],
