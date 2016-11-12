7149bf1cf2562064952227f09fb8b7cfba0672cb
Keith J. Jones <keith@keithjjones.com>
2016-11-11 22:12:11 -0500

    Made smaller graphs with dot.

--- scripts/generateransomwaregallery.sh
+++ scripts/generateransomwaregallery.sh
@@ -4,7 +4,7 @@ plotprocmoncsv -sp -t "Ransomware Example 3" -pt -pu -sh -f ../gallery/procmoncs
 plotprocmoncsv -t "Ransomware Example 4" -pfw -pfd -pfn -sp -f ../gallery/procmoncsv/ransomware_example4.html /Source/Procmon\ CSV/Ransomware_focused.csv
 plotprocmoncsv -t "Ransomware Example 5" -pfw -pfd -pfn -sp -f ../gallery/procmoncsv/ransomware_example5.html /Source/Procmon\ CSV/Ransomware.csv
 plotprocmoncsv -t "Ransomware Example 6" -prw -prd -sp -f ../gallery/procmoncsv/ransomware_example6.html /Source/Procmon\ CSV/Ransomware_focused.csv
-plotcuckoojson -t "Ransomware Example 1" -f ../gallery/cuckoojson/ransomware_example1.html -fa -na -ra /Source/cuckoo-modified-json/3_report.json
+plotcuckoojson -t "Ransomware Example 1" -f ../gallery/cuckoojson/ransomware_example1.html -fa -na -ra -gp dot /Source/cuckoo-modified-json/3_report.json
 plotcuckoojson -t "Ransomware Example 2" -f ../gallery/cuckoojson/ransomware_example2.html -fa -ra /Source/cuckoo-modified-json/3_report.json
 plotcuckoojson -t "Ransomware Example 3" -f ../gallery/cuckoojson/ransomware_example3.html -na -ra /Source/cuckoo-modified-json/3_report.json
 plotcuckoojson -t "Ransomware Example 4" -f ../gallery/cuckoojson/ransomware_example4.html -na -fa /Source/cuckoo-modified-json/3_report.json
